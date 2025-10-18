271df1e44e1d2db95ffc296f2b8dee047391236e
InfinityC0der <ayusinconnect@gmail.com>
2025-10-18 13:20:13 -0400

    Update response.py

--- response.py
+++ response.py
@@ -1,7 +1,7 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
 
-modelName = "EleutherAI/gpt-neo-125M"
+modelName = "EleutherAI/gpt-neo-125M"  #Grabbed off of huggingface documentation:(https://huggingface.co/docs/transformers/quicktour)
 tokenizer = AutoTokenizer.from_pretrained(modelName)
 model = AutoModelForCausalLM.from_pretrained(modelName)
 
@@ -23,17 +23,11 @@ def response(wish, action):
     f"You are a mischievous genie{twistStrat}"
     f"Use 1-2 sentences. \nUser's wish: {wish}\nGenie's reply:"
     )
-    inputIDs = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+    inputIDs = tokenizer(prompt, return_tensors="pt").input_ids.to(device) #Grabbed off of huggingface documentation:(https://huggingface.co/docs/transformers/model_doc/gpt2#quick-tour)
 
     outputIDs = model.generate(inputIDs, max_length=200, do_sample=True, temperature=0.9, top_p=0.95, eos_token_id=tokenizer.eos_token_id)
     responseText = tokenizer.decode(outputIDs[0], skip_special_tokens=True)
 
-    stop_token = "User"  # anything after this will be removed
-    if stop_token in responseText:
-        responseText = responseText.split(stop_token)[0].strip()
-
-    words = responseText.split()
-    responseText = " ".join(dict.fromkeys(words))
-
     return responseText
+
 
