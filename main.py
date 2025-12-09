fbc9a2be0b25552ab35baaa26529386b98839830
InfinityC0der <ayusinconnect@gmail.com>
2025-12-08 23:17:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-from rl_agent import chooseAction, update_q_table, get_feedback, qTable, load_q_table, save_q_table
+from rl_agent import chooseAction, update_q_table, get_feedback, load_q_table, save_q_table, get_q_table
 from rl_agent import load_epsilon, save_epsilon, get_epsilon, decay_epsilon
 from cat import categorize
 import response
 epsilon = 1
 load_q_table()
+qTable = get_q_table()
 load_epsilon()
 def main():
     print("Hello user, I am a genie trapped in this computer and I'll grant you one wish") #Perhaps add an ASCll genie later for images
     print("But as they say, be careful what you wish for!")
-    wish1 = input("Make your 1st wish:\n")
-
-    
+    wish1 = input("Make your 1st and last wish:\n")
     stateInfo = categorize(wish1)
     categoryName = stateInfo["category"]  #wish category is also the states
     confidence = stateInfo["confidence"]
-    print(categoryName, confidence)            #The model is so insanely slow that I may need to put all three wishes together separated by a hash symbol or something
     epsilon = get_epsilon()
     action = chooseAction(categoryName, qTable, epsilon) #Choose twist strategy based on the wish category
     response1 = response.response(wish1, action)      #Generate response
